2a0a6ed29cdae68e0dd3b6e56499c9dbfdea61b9
minsitt101 <158995368+minsitt101@users.noreply.github.com>
2024-06-02 01:12:51 -0700

    a lot of stuff, working on Ecobear

---
+++ assets/Project Page css & images/Personal Portfolio_files/homepage.js
@@ -0,0 +1,32 @@
+function scrollToTop() {
+    // Scroll to the top of the page
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Smooth scrolling animation
+    });
+  }
+
+var scrollBtn = document.getElementById("scrollBtn");
+
+// Show the button when scrolling down, hide it when scrolling to top
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
